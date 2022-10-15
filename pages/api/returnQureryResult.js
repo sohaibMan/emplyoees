89714a37d9b287270e9b8207da8807bc7c235d9f
SohaibMan <souhaibemanah@gmail.com>
2022-10-15 11:41:38 +0100

    changing the database

--- pages/api/returnQureryResult.js
+++ pages/api/returnQureryResult.js
@@ -2,14 +2,14 @@ import mysql from 'mysql2/promise'
 export default  async function returnQureryResult(query) {
     try {
         const connection = await mysql.createConnection({
-            // host: 'localhost',
-            // user: 'root',
-            // password:'123jkl;',
-            // database: 'mangement' ,
-            host: 'sql201.epizy.com',
-            user: 'epiz_32795820',
-            password:'ZGTyv7qKhIGZK;',
-            database: 'epiz_32795820_employees'
+            host: 'localhost',
+            user: 'root',
+            password:'123jkl;',
+            database: 'mangement' ,
+            // host: 'sql201.epizy.com',
+            // user: 'epiz_32795820',
+            // password:'ZGTyv7qKhIGZK;',
+            // database: 'epiz_32795820_employees'
         });
       
         const [queryResult]= await  connection.execute(query,[]);
